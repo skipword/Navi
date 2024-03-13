34085114792400711f1de0cc37f9f520c7ffc7ae
Oscar Mauricio Mongui Piñeros <126714183+OskrMongui11@users.noreply.github.com>
2024-03-13 09:48:37 -0500

    Update Mult.asm

--- proyectos/04/Mult.asm
+++ proyectos/04/Mult.asm
@@ -1,27 +1,27 @@
 @R2
-M=0
+M=0  // Inicializa R2 a 0
 
 @R0
-D=M
+D=M  // Carga el valor de R0 en D
 @STEP
-D;JGT
+D;JGT  // Salta a STEP si R0 es mayor que 0
 
 @END
-0;JMP
+0;JMP  // Salta a END
 
 (STEP)
     @R2
-    D=M
+    D=M  // Carga el valor de R2 en D
     @R1
-    D=D+M
+    D=D+M  // Suma el valor de R1 a D
     @R2
-    M=D
+    M=D  // Guarda el resultado en R2
     @R0
-    D=M-1
-    M=D
+    D=M-1  // Decrementa el valor de R0
+    M=D  // Guarda el nuevo valor de R0
     @STEP
-    D;JGT
+    D;JGT  // Salta a STEP si R0 es mayor que 0
 
 (END)
     @END
-    0;JMP
+    0;JMP  // Salta a END
